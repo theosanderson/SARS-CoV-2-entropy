2c7251e92be4742b7f41f0a68a6bf0d33c45de9a
theosanderson <theo@theo.io>
2022-10-19 15:15:54 +0100

    updates

---
+++ run.sh
@@ -0,0 +1,8 @@
+
+pip install biopython alive-progress tqdm pandas 
+# protobuf
+pip install protobuf
+# convert entropy.ipynb to entropy.py
+jupyter nbconvert --to script entropy.ipynb
+# run entropy.py
+python entropy.py
